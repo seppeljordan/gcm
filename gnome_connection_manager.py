02e0df0c2cc92142fa2d63a5eb33ec03e79f4de9
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2014-05-09 12:49:16 +0200

    Move EntryDialog to msgDialogs module
    The EntryDialog class lives now in the msgDialogs.py module.  Its only
    purpose was to define a dialog for the inputbox.  Also added some
    docstrings to inputbox and EntryDialog

--- gnome_connection_manager.py
+++ gnome_connection_manager.py
@@ -2781,52 +2781,6 @@ class NotebookTabLabel(gtk.HBox):
         elif event.type == gtk.gdk.BUTTON_PRESS and event.button == 2:    
             self.close_tab(self.widget)
 
-class EntryDialog( gtk.Dialog):
-    def __init__(self, title, message, default_text='', modal=True, mask=False):
-        gtk.Dialog.__init__(self)
-        self.set_title(title)
-        self.connect("destroy", self.quit)
-        self.connect("delete_event", self.quit)
-        if modal:
-            self.set_modal(True)
-        box = gtk.VBox(spacing=10)
-        box.set_border_width(10)
-        self.vbox.pack_start(box)
-        box.show()
-        if message:
-            label = gtk.Label(message)
-            box.pack_start(label)
-            label.show()
-        self.entry = gtk.Entry()
-        self.entry.set_text(default_text)
-        self.entry.set_visibility(not mask)
-        box.pack_start(self.entry)
-        self.entry.show()
-        self.entry.grab_focus()
-        button = gtk.Button(stock=gtk.STOCK_OK)
-        button.connect("clicked", self.click)
-        self.entry.connect("activate", self.click)
-        button.set_flags(gtk.CAN_DEFAULT)
-        self.action_area.pack_start(button)
-        button.show()
-        button.grab_default()
-        button = gtk.Button(stock=gtk.STOCK_CANCEL)
-        button.connect("clicked", self.quit)
-        button.set_flags(gtk.CAN_DEFAULT)
-        self.action_area.pack_start(button)
-        button.show()
-        self.ret = None
-
-    def quit(self, w=None, event=None):
-        self.hide()
-        self.destroy()        
-
-    def click(self, button):
-        self.value = self.entry.get_text()        
-        self.response(gtk.RESPONSE_OK)
-
-
-
 class CellTextView(gtk.TextView, gtk.CellEditable):
 
     __gtype_name__ = "CellTextView"
--- msgDialogs.py
+++ msgDialogs.py
@@ -16,12 +16,64 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import gtk
-from gnome_connection_manager import EntryDialog, ICON_PATH
+import os
+
+ICON_PATH = os.path.dirname(os.path.abspath(__file__)) + "/icon.png"
+
+class EntryDialog( gtk.Dialog):
+    """Create a Window that prompts the user to enter text."""
+    def __init__(self, title, message, default_text='', modal=True, mask=False):
+        gtk.Dialog.__init__(self)
+        self.set_title(title)
+        self.connect("destroy", self.quit)
+        self.connect("delete_event", self.quit)
+        if modal:
+            self.set_modal(True)
+        box = gtk.VBox(spacing=10)
+        box.set_border_width(10)
+        self.vbox.pack_start(box)
+        box.show()
+        if message:
+            label = gtk.Label(message)
+            box.pack_start(label)
+            label.show()
+        self.entry = gtk.Entry()
+        self.entry.set_text(default_text)
+        self.entry.set_visibility(not mask)
+        box.pack_start(self.entry)
+        self.entry.show()
+        self.entry.grab_focus()
+        button = gtk.Button(stock=gtk.STOCK_OK)
+        button.connect("clicked", self.click)
+        self.entry.connect("activate", self.click)
+        button.set_flags(gtk.CAN_DEFAULT)
+        self.action_area.pack_start(button)
+        button.show()
+        button.grab_default()
+        button = gtk.Button(stock=gtk.STOCK_CANCEL)
+        button.connect("clicked", self.quit)
+        button.set_flags(gtk.CAN_DEFAULT)
+        self.action_area.pack_start(button)
+        button.show()
+        self.ret = None
+
+    def quit(self, w=None, event=None):
+        self.hide()
+        self.destroy()        
+
+    def click(self, button):
+        self.value = self.entry.get_text()        
+        self.response(gtk.RESPONSE_OK)
+
 
 def inputbox(title, text, default='', password=False):
-	"""
-	This procedure creates a small inputbox for user interaction.
-	"""
+	"""This procedure creates a small inputbox for user interaction.
+
+        Arguments: 
+        title -- string that will be displayed as window title 
+        text -- string that will be displayed to the user as a prompt
+        password -- boolean that defines if the input of the user will be masked
+        """
 	msgBox = EntryDialog(title, text, default, mask=password)
 	msgBox.set_icon_from_file(ICON_PATH)
 	if msgBox.run() == gtk.RESPONSE_OK:
